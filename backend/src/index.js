379d6997c995f80bb09259f6a8422319a28d3ee7
Jose Eduardo <joseduardosilva.42@gmail.com>
2021-09-04 00:06:07 -0300

    initial

---
+++ backend/src/index.js
@@ -0,0 +1,31 @@
+const { response } = require('express');
+const express = require('express');
+
+const app = express();
+
+app.get('/projects', (request, response) => {
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+  ]);
+});
+
+app.post('/projects', (request, response) => {
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+})
+
+app.post('/projects', (request, response) => {
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+})
+
+app.listen(3333, () => {
+  console.log('funcionando');
+});
